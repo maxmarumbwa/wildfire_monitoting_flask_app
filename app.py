2916e4644d035e539cddabe8162738738925d3af
maxmarumbwa <maxmarumbwa@gmail.com>
2025-12-11 16:22:55 +0200

    fwi/place - names in code

--- app.py
+++ app.py
@@ -85,7 +85,8 @@ def fwi_place():
     lat, lon = zimbabwe_coords[place]
 
     # Call your weather API
-    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={os.getenv('API_KEY')}"
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
+
     response = requests.get(url)
 
     return jsonify(response.json())
